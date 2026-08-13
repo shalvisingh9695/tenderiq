427087685342c227110833b1eb834fa0cef10559
Shalvi Singh <shalvisingh9695@gmail.com>
2026-08-13 14:17:46 +0530

    Fix backend deploy error

--- server.js
+++ server.js
@@ -40,10 +40,9 @@ async function startServer() {
     app.use(vite.middlewares);
   } else {
     const distPath = path.join(process.cwd(), 'dist');
-    app.use(express.static(distPath));
-    app.get('*', (req, res) => {
-      res.sendFile(path.join(distPath, 'index.html'));
-    });
+    app.get("/", (req, res) => {
+  res.send("TenderIQ Backend Running 🚀");
+});
   }
 
   app.listen(PORT, '0.0.0.0', () => {
